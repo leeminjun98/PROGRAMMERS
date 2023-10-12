0f28518f66a9d2c4298aa913cb94aa8ae1a53fb8
Min Jun Lee <100201201+leeminjun98@users.noreply.github.com>
2023-10-12 23:32:12 +0900

    Update x만큼 간격이 있는 n개의 숫자.java

--- lv. 1/x만큼 간격이 있는 n개의 숫자.java
+++ lv. 1/x만큼 간격이 있는 n개의 숫자.java
@@ -2,13 +2,13 @@ class Solution {
     public long[] solution(int x, int n) {
         
         long result[]=new long[n];
-        long add = x;
+        long add = x;   //x가 나중에는 커져서 int type을 벗어날 수 있는거 주의!!
         for(int i=0;i<n;i++){
             result[i]=(i+1)*add;
             }
         
         return result;
-        
-        
     }
 }
+
+
