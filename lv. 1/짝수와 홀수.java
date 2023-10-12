4eaeb7914ac39a3baa5a716d0a148887ab080394
Min Jun Lee <100201201+leeminjun98@users.noreply.github.com>
2023-10-12 23:13:24 +0900

    Create 짝수와 홀수.java

---
+++ lv. 1/짝수와 홀수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(int num) {
+        
+        String Even = "Even", Odd = "Odd";
+        
+        if(num%2==0){
+            return Even;
+        }
+        else
+            return Odd;
+        
+    }
+}
