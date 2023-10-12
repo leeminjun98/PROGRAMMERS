50f59cb9e76cc273fc0f70f72b6be17d851dc8e0
Min Jun Lee <100201201+leeminjun98@users.noreply.github.com>
2023-10-13 00:31:18 +0900

    Update 자연수 뒤집어 배열로 만들기.java

--- lv. 1/자연수 뒤집어 배열로 만들기.java
+++ lv. 1/자연수 뒤집어 배열로 만들기.java
@@ -15,3 +15,5 @@ class Solution {
         return result;
     }
 }
+//다른 사람들이 한걸 보면, n의 자릿수를 구할때 -> 자연수를 문자열로 바꾸는 string.valueOf(n)을 사용하고, 그 스트링의 길이로 자릿수를 알아낸다. 
+//string s = "" +n;을 이용해서도 자릿수구할수 있다.
