3ea57b1911ad7a9c44c02e54cdb609de2a85e5b1
Min Jun Lee <100201201+leeminjun98@users.noreply.github.com>
2023-10-13 10:06:45 +0900

    Create 정수 제곱근 판별.java

---
+++ lv. 1/정수 제곱근 판별.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(long n) {
+        
+        if(n==1)
+            return 4;
+        for(long x = 1; x<n;x++){
+            if(x*x==n){
+                return (x+1)*(x+1);
+            }
+        }
+        return -1;
+    }
+}
