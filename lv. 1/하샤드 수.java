5ef52e4198d03176268ef0d0f01b62290da2906d
Min Jun Lee <100201201+leeminjun98@users.noreply.github.com>
2023-10-14 11:16:52 +0900

    Create 하샤드 수.java

---
+++ lv. 1/하샤드 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum=0;
+        int num =x;
+        while(num>0){
+            sum += num%10;
+            num/=10;      
+        }
+        if((x%sum)==0){
+            return true;
+        }
+        else
+            return false;
+        
+    }
+}
