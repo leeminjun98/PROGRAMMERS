75c71681fc5d01530e5daa19cc25dfb437906c98
Min Jun Lee <100201201+leeminjun98@users.noreply.github.com>
2023-11-02 11:12:58 +0900

    Create 33. 예산.java

---
+++ lv. 1/33. 예산.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] d, int budget) {
+        
+        Arrays.sort(d);
+        int count=0;
+        for(int i=0;i<d.length;i++){
+            if((budget-d[i])>=0){
+                budget-=d[i];
+                count++;
+            }
+            else
+                return count;
+        }
+        
+        return count;
+    
+    }
+}
